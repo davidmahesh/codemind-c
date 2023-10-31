94d93b035357950a29b644ffe41e2059d66381da
davidmahesh <147488196+davidmahesh@users.noreply.github.com>
2023-10-31 21:25:07 +0530

    #include<stdio.h> int main() {     int i,n;     scanf("%d",&n);     i=1;     while(i<=n)     {         if(i%2==1)         {            printf("%d ",i);         }         i++;     }
    }

---
+++ Odd Natural_Numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    scanf("%d",&n);
+    i=1;
+    while(i<=n)
+    {
+        if(i%2==1)
+        {
+           printf("%d ",i);
+        }
+        i++;
+    }
+    
+    
+}
