46f6d1f026cbe05f76605a5cbfae793cf0120b32
davidmahesh <147488196+davidmahesh@users.noreply.github.com>
2023-11-01 16:07:55 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     if(n=4*n<=1000)     {         printf("YES");     }     else     {         printf("NO");     }
    }

---
+++ Tickets.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n=4*n<=1000)
+    {
+        printf("YES");
+    }
+    else 
+    {
+        printf("NO");
+    }
+    
+    
+    
+    
+}
