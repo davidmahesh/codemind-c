6f75ba5e3b1fcaf9f675493c95024066425175b6
davidmahesh <147488196+davidmahesh@users.noreply.github.com>
2023-11-06 15:55:30 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     if(n%2==1)     {         printf("Prime");     }     else     {         printf("Not Prime");     }
    }

---
+++ Prime.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%2==1)
+    {
+        printf("Prime");
+    }
+    else
+    {
+        printf("Not Prime");
+    }
+    
+    
+    
+}
