cec88d409020415419b8271a69a7d5181b85ff47
davidmahesh <147488196+davidmahesh@users.noreply.github.com>
2023-11-22 15:28:14 +0530

    #include<stdio.h> int main() {     char alp;     scanf("%c",&alp);     if(alp=='a' || alp=='e' || alp=='i' || alp=='o' || alp=='u'||alp=='A' || alp=='E' || alp=='I' || alp=='O' || alp=='U')     {         printf("VOWEL");     }     else     {         printf("CONSONANT");     }
    }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    char alp;
+    scanf("%c",&alp);
+    if(alp=='a' || alp=='e' || alp=='i' || alp=='o' || alp=='u'||alp=='A' || alp=='E' || alp=='I' || alp=='O' || alp=='U')
+    {
+        printf("VOWEL");
+    }
+    else 
+    {
+        printf("CONSONANT");
+    }
+    
+    
+    
+    
+}
