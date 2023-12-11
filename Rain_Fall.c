c00e34c5863cb5b18bcda7be58cef9c37d774b27
davidmahesh <147488196+davidmahesh@users.noreply.github.com>
2023-12-11 19:10:08 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     if(n<3)     {         printf("LIGHT");     }     else if(n>=3 && n<7)     {         printf("MODERATE");     }     else     {         printf("HEAVY");     }
    }

---
+++ Rain_Fall.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n<3)
+    {
+        printf("LIGHT");
+    }
+    else if(n>=3 && n<7)
+    {
+        printf("MODERATE");
+    }
+    else
+    {
+        printf("HEAVY");
+    }
+    
+    
+    
+}
