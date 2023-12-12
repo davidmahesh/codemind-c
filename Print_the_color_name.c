9be7c8b9a71886768cc1cb1477f493cfde57b3cd
davidmahesh <147488196+davidmahesh@users.noreply.github.com>
2023-12-12 18:15:02 +0530

    #include<stdio.h> int main() {     char alp,V,I,B,G,Y,O,R;     scanf("%c",&alp);     if(alp=='V')     {         printf("Violet");     }     else if(alp=='I')     {         printf("Indigo");     }     else if(alp=='B')     {         printf("Blue");     }     else if(alp=='G')     {         printf("Green");     }     else if(alp=='Y')     {         printf("Yellow");     }     else if(alp=='O')     {         printf("Orange");     }     else if(alp=='R')     {         printf("Red");     }     else     {         printf("-1");     }
    }

---
+++ Print_the_color_name.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+    char alp,V,I,B,G,Y,O,R;
+    scanf("%c",&alp);
+    if(alp=='V')
+    {
+        printf("Violet");
+    }
+    else if(alp=='I')
+    {
+        printf("Indigo");
+    }
+    else if(alp=='B')
+    {
+        printf("Blue");
+    }
+    else if(alp=='G')
+    {
+        printf("Green");
+    }
+    else if(alp=='Y')
+    {
+        printf("Yellow");
+    }
+    else if(alp=='O')
+    {
+        printf("Orange");
+    }
+    else if(alp=='R')
+    {
+        printf("Red");
+    }
+    else
+    {
+        printf("-1");
+    }
+    
+    
+    
+    
+}
